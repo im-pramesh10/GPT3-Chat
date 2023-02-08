fb6eedcd9351261ec7618378e9007a49f67750fc
Pramesh Mahat <cruisepramesh@gmail.com>
2023-02-08 23:18:57 +0545

    api request and response

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+require('dotenv').config();
 const express = require('express');
-const openai = require('openai');
-const configuration = new openai.Configuration({
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
+const openai = new OpenAIApi(configuration);
 const app = express();
 const port = 5000;
 
-app.listen(port)
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.json({'name': 'pramesh'})
+app.get('/', async (req, res)=>{
+
+    const openairesponse= await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "You are a chatbot. Now, you will only talk to me as a human. The things that come after it will be my chats. what are you sure of?",
+        temperature: 0.8,
+        max_tokens: 500,
+      });
+    
+      console.log(openairesponse.data.choices[0].text);
+      res.send(openairesponse.data.choices[0].text);
+})
+
+// app.get('/h', (req,res)=>{
+//     res.send('hello');
+// })
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`)
 })
