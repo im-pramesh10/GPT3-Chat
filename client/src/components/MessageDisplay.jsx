7f4bf07fbb10f8c860da4ef699142197f9d4c5c0
Pramesh Mahat <cruisepramesh@gmail.com>
2023-02-09 18:32:41 +0545

    chat display skeleton

--- client/src/components/MessageDisplay.jsx
+++ client/src/components/MessageDisplay.jsx
@@ -1,7 +1,22 @@
 import styles from "./MessageDisplay.module.css";
+import logo from "../assets/Robot.svg";
 
 export default function MessageDisplay() {
     return (
-        <div className={styles.displaywrapper}></div>
+        <div className={
+            styles.displaywrapper
+        }>
+            <div className={
+                styles.userquery
+            }>
+                <p className={styles.queryItem}>hello</p>
+            </div>
+            <div className={
+                styles.gptresponse
+            }>
+            <img className={styles.resIcon} src={logo}/>
+                <p>gptResponse</p>
+            </div>
+        </div>
     );
 }
